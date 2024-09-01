d3d0126cd868de7742f79634ad8a5504d78656cc
sonis3790 <126348520+sonis3790@users.noreply.github.com>
2024-09-01 12:52:19 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert( 3 + 3) // we are using node.js not a browser there have another way to use alert.
+
+console.log(3 
+    + 
+    3)// code redibility should be higher
+
+let name = "shourya"
+let age = 18
+let loggedIn = false
+
+        // Primitive dataTypes
+// number 2 to 53
+// bigint
+// string => ""
+// null => standalone value
+// boolean => true/false
+// undefined => not assigned value
+// symbol => uniqueness in code using mostly in react
+
+// object
+
+console.log(typeof null);       // object
+console.log(typeof undefined);  // undefined
+
