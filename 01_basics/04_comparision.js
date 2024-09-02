00ac4a3b1ebaa52577144a4896933d9c4bd95f92
sonis3790 <126348520+sonis3790@users.noreply.github.com>
2024-09-02 18:08:28 +0000

    comparision done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("01" > 1);
+        // == checking value only
+console.log(null == 0); // the reason that is an equality check == and comparisons >, <, >= ,<= work differently
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null <= 0);
+
+console.log(undefined == 0);
+
+// === checking value and datatypes
+
+console.log( 2 === 2);
+console.log("2" === 2);
+
+
+
