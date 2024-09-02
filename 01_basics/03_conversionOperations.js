a1ec60f056c4eff79d40360b72ea9f0ca0ce1175
sonis3790 <126348520+sonis3790@users.noreply.github.com>
2024-09-02 13:30:14 +0000

    conversion done

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,28 @@
+let score = null;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// "33" => 33 give a number
+// "33ab" => NaN but type is  (Number)
+// true => 1/false => 0
+
+// conversion number into boolean
+
+let loggedIn = "";
+
+let outputLoggedIn = Boolean(loggedIn);
+//console.log(outputLoggedIn);
+// 1 => true; 0 => false;
+// "" => false;
+// "shourya" put string value = true;
+
+let str = 33;
+
+let stringNumber = String(str);
+console.log(stringNumber);
+console.log(typeof stringNumber);
