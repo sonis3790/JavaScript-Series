3ef49bc6b0b26f3f7e04bc7a98b5aeffb8bbf654
sonis3790 <126348520+sonis3790@users.noreply.github.com>
2024-09-03 18:16:47 +0000

    math and num done

---
+++ 01_basics/06_maths_and_nums.js
@@ -0,0 +1,38 @@
+const score = 400;
+//console.log(score);
+
+const balance = new Number(100);
+//console.log(balance);
+
+//console.log(balance.toString());
+//console.log(balance.toFixed(3));  // using vlaue fixed in decimal to ecommerce site
+
+const otherNumber = 623.8394750;
+//console.log(otherNumber.toPrecision(2)); // using roundfigure value to decimal 
+
+const thousands = 10000000;
+//console.log(thousands.toLocaleString('en-IN')); //using indian standards or empty to us standards
+
+// +++++++++++++++ Maths +++++++++++++++++++++ //
+
+// console.log(Math);
+// console.log(Math.abs(-9));
+// console.log(Math.round(-9.6));
+// console.log(Math.ceil(-9.5));
+// console.log(Math.floor(-9.9));
+// console.log(Math.min(4,7,3,2,9));
+// console.log(Math.max(-9,-20,));
+
+console.log(Math.random()) // 0 or 1
+console.log((Math.random() * 10)+ 1) ; // 1.23 to 9.488900
+console.log(Math.floor(Math.random() * 10) + 1);  // 1 to 10
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min +1)) + min);
+
+
+
+
+
