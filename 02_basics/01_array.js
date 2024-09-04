ab5a444a9538b5758c71282b448e8ab0c12b46fa
sonis3790 <126348520+sonis3790@users.noreply.github.com>
2024-09-04 18:22:04 +0000

    array basics part 1

---
+++ 02_basics/01_array.js
@@ -0,0 +1,48 @@
+            
+                        // The Array object, as with arrays in other programming languages, 
+     //array =>        //enables storing a collection of multiple items under a single variable name, 
+                        //and has members for performing common array operations.
+
+const myArr = [0,1,2,3,5,6]
+const heroes = ["captain America","Hulk","Ironman"];
+
+const myArr2 = new Array(1,2,24,9,2);
+//console.log(myArr);
+//console.log(myArr[4]);
+
+// ++++++++++++ Array methods +++++++++++++++++++++++ //
+// myArr.push(7);   // add last element
+// myArr.pop();   // delete last element
+// myArr.push(56);
+
+
+//myArr.unshift(32);  // add first delete
+// myArr.shift();   // delete first element
+
+ 
+// console.log(myArr.includes(7));   // check the certain element in array are available ans. true & false
+
+// console.log(myArr.indexOf(8));  // check the value and output -ve in -1
+
+const newArr = myArr.join();  // output in string format
+
+// console.log(myArr);
+// console.log(newArr);  
+
+// console.log("A", myArr);
+
+// const myn1 = myArr.slice(1,3);
+// console.log(myn1);
+
+// console.log("B", myArr);
+
+// console.log("A", myArr);
+
+// const myn2 = myArr.splice(1,3);
+// console.log(myn2);
+
+// console.log("B", myArr);
+// console.log(myn2);
+
+
+
