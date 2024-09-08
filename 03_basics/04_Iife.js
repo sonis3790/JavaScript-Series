0869a2e8c03b22df432feff3263f16d3eea5cc2a
sonis3790 <126348520+sonis3790@users.noreply.github.com>
2024-09-08 19:04:37 +0000

    done

---
+++ 03_basics/04_Iife.js
@@ -0,0 +1,18 @@
+//    Immediately invoked function expressions(IIFE)
+
+// it is use for global scope pollution problem to clear process name is Iife.
+
+// (function chai(){
+//         // named Iife
+//     console.log("DB CONNECTED");
+// })();
+// ( function definaton )
+// (function execution)
+ chai()
+
+// ((name ) => {    
+//     console.log(`DB Connected two ${name}`);
+// })("balram");
+
+//
+
